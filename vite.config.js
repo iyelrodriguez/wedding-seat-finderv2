ee58b2db30a350d9df63e3645194aaf5a0750afb
iyelrodriguez <108468335+iyelrodriguez@users.noreply.github.com>
2025-09-10 09:30:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/wedding-seat-finderv2/", // 👈 add this
+  base: "./", // 👈 ensures relative paths work
 });
